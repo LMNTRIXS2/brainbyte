0bd53371e8b118e8c95d474a4dd5ce5624971859
Robertnant <robertnantchouangn@gmail.com>
2020-04-22 19:13:10 +0200

    Push version 2 of website

--- js/js1.js
+++ js/js1.js
@@ -1,21 +1,201 @@
-function($) { 
-  $(function() { 
-    
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
-    });
-    
-    $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
-    });
-    $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
-    });
-  }); 
-})(jQuery); 
+
+        class InfiniteSlider {
+	constructor(animTime = '10000', selector = '.slider', container = '#slider-container') {
+		this.slider = document.querySelector(selector)
+		this.container = document.querySelector(container)
+		this.width = 0
+		this.oldWidth = 0
+		this.duration = parseInt(animTime)
+		this.start = 0
+		this.refresh = 0 //0, 1, or 2, as in steps of the animation
+		this._prevStop = false
+		this._stop = false
+		this._oldTimestamp = 0
+	}
+	
+	animate() {
+		/* fix for browsers who like to run JS before images are loaded */
+		const imgs = Array.prototype.slice.call(this.slider.querySelectorAll('img'))
+						.filter(img => {
+							return img.naturalWidth === 0
+						})
+		if (imgs.length > 0) {
+			window.requestAnimationFrame(this.animate.bind(this));
+			return
+		}
+		
+		/* Add another copy of the slideshow to the end, keep track of original width */
+		this.oldWidth = this.slider.offsetWidth
+		const sliderText = '<span class="slider-extra">' + this.slider.innerHTML + '</span>'
+		this.slider.innerHTML += sliderText
+
+		/* can have content still when we move past original slider */
+		this.width = this.slider.offsetWidth
+		const minWidth = 2 * screen.width
+
+		/* Add more slideshows if needed to keep a continuous stream of content */
+		while (this.width < minWidth) {
+			this.slider.innerHTML += sliderText
+			this.width = this.slider.width
+		}
+		this.slider.querySelector('.slider-extra:last-child').classList.add('slider-last')
+		
+		/* loop animation endlesssly (this is pretty cool) */
+		window.requestAnimationFrame(this.controlAnimation.bind(this))
+	}
+	
+	halt() {
+		this._stop = true
+		this._prevStop = false
+	}
+	
+	go() {
+		this._stop = false
+		this._prevStop = true
+	}
+	
+	stagnate() {
+		this.container.style.overflowX = "scroll"
+	}
+	
+	controlAnimation(timestamp) {
+		//console.log('this.stop: ' + this._stop + '\nthis.prevStop: ' + this._prevStop)
+		if (this._stop === true) {
+			if (this._prevStop === false) {
+				this.slider.style.marginLeft = getComputedStyle(this.slider).marginLeft
+				this._prevStop = true
+				this._oldTimestamp = timestamp
+			}
+		} else if (this._stop === false && this._prevStop === true) {
+			this._prevStop = false
+			this.start = this.start + (timestamp - this._oldTimestamp)
+		} else {
+			//reset animation
+			if (this.refresh >= 1) {
+				this.start = timestamp
+				this.slider.style.marginLeft = 0
+				this.refresh = 0
+				window.requestAnimationFrame(this.controlAnimation.bind(this))
+				return
+			}
+			if (timestamp - this.start >= this.duration) {
+				this.refresh = 1
+			}
+			
+			const perc = ((timestamp - (this.start)) / this.duration) * this.oldWidth
+			this.slider.style.marginLeft = (-perc) + 'px'
+		}
+		window.requestAnimationFrame(this.controlAnimation.bind(this))
+		return
+	}
+	
+	getIeWidth() {
+		this.slider.style.marginLeft = '-99999px';
+	}
+	
+	ie11Fix() {
+		this.slider.querySelector('.slider-last').style.position = 'absolute';
+	}
+}
+
+function detectIE() {
+	var ua = window.navigator.userAgent
+	var msie = ua.indexOf('MSIE ')
+
+	if (msie > 0) {
+		// IE 10 or older => return version number
+		return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10)
+	}
+	
+	var trident = ua.indexOf('Trident/')
+	if (trident > 0) {
+		// IE 11 => return version number
+		var rv = ua.indexOf('rv:')
+		return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10)
+	}
+
+	var edge = ua.indexOf('Edge/');
+	if (edge > 0) {
+		// Edge (IE 12+) => return version number
+		return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10)
+	}
+
+	// other browser
+	return false
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	const slider = new InfiniteSlider(20000)
+	const ie = detectIE()
+	
+	//Dont animate under IE10, just place the images
+	if (ie !== false && ie < 10) {
+		slider.stagnate()
+		return
+	}
+	//IE 11 and lower, fix for width *increasing* as more of the slider is shown
+	if (ie !== false && ie < 12) { slider.getIeWidth() }
+	
+	slider.animate()
+	document.querySelector('#slider-container')
+		.addEventListener('mouseenter', slider.halt.bind(slider))
+	document.querySelector('#slider-container')
+		.addEventListener('mouseleave', slider.go.bind(slider))
+	
+	if (ie === 11) {
+		setTimeout(slider.ie11Fix.bind(slider), 1000)
+	}
+});
+
+
+
+$(".login-form").hide();
+$(".login").css("background", "none");
+
+$(".login").click(function(){
+  $(".signup-form").hide();
+  $(".login-form").show();
+  $(".signup").css("background", "#fff");
+  $(".login").css("background", "#e9eaea");
+});
+
+$(".signup").click(function(){
+  $(".signup-form").show();
+  $(".login-form").hide();
+  $(".login").css("background", "#fff");
+  $(".signup").css("background", "#e9eaea");
+});
+
+$(".btn").click(function(){
+  $(".input").val("");
+});
+        
+        	$(document).ready(function(){
+
+		var showHeaderAt = 150;
+
+		var win = $(window),
+				body = $('body');
+
+		// Show the fixed header only on larger screen devices
+
+		if(win.width() > 400){
+
+			// When we scroll more than 150px down, we set the
+			// "fixed" class on the body element.
+
+			win.on('scroll', function(e){
+
+				if(win.scrollTop() > showHeaderAt) {
+					body.addClass('fixed');
+				}
+				else {
+					body.removeClass('fixed');
+				}
+			});
+
+		}
+
+	});  
+
